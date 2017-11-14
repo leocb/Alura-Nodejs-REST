08adc3f8792afb54805e2d3cc97f04051c8673d4
leocb <leobottaro@gmail.com>
2017-11-14 14:49:59 -0200

    Aula 5.2 Cliente para memcached

---
+++ servicos/memcachedClient.js
@@ -0,0 +1,22 @@
+let memcached = require('memcached')
+
+let cliente = new memcached('localhost:11211', {
+    retries: 10,
+    retry: 10000,
+    remove: true
+})
+
+cliente.set('pagamento-3', {
+    id: 20
+}, 60000, function (erro) {
+    console.log('nova chave adicionada! - pagamento-2')
+})
+
+cliente.get('pagamento-3', function (erro, retorno) {
+    if (erro || !retorno) {
+        console.log('MISS - ' + JSON.stringify(erro))
+        return
+    }
+
+    console.log('HIT - ' + JSON.stringify(retorno))
+})
