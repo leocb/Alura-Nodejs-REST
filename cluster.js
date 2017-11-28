6adc5cc698c7075e694e19bf1fefa1863794c3c2
leocb <leobottaro@gmail.com>
2017-11-28 15:39:24 -0200

    Aula 7.3 Listeners no cluster

--- cluster.js
+++ cluster.js
@@ -4,11 +4,23 @@ let os = require('os')
 let cpus = os.cpus()
 
 console.log('executando thread')
+
 if (cluster.isMaster) {
     console.log('master thread');
+
     cpus.forEach(cpu => {
         cluster.fork()
     });
+
+    cluster.on('listening', worker => {
+        console.log('Cluster conectado ' + worker.process.pid);
+    })
+
+    cluster.on('exit', worker => {
+        console.log('cluster %d desconectado', worker.process.pid);
+        cluster.fork()
+    })
+
 } else {
     console.log('slave thread');
     require('./index')
